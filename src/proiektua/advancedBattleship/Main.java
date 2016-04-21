ceacf579c660763ef0f7da1b9e834c9514cbe4e2
Ibon Lopez <ibonlb96@gmail.com>
2016-04-21 15:11:05 +0200

    Tableroa

--- src/proiektua/advancedBattleship/Main.java
+++ src/proiektua/advancedBattleship/Main.java
@@ -8,7 +8,9 @@ public abstract class Main {
 			
 			System.out.print(x + "|▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢▢|\n");
 		}*/
-		AdvancedBattleship.getAdvancedBattleship().jokatu();
+		//AdvancedBattleship.getAdvancedBattleship().jokatu();
+		Tableroa tablero = new Tableroa();
+		tablero.getTableroa();
 	}
 
 }
