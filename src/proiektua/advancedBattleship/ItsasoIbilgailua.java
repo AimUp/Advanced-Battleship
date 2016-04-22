eb554e9366dd5c29a7bb5874a7dbcf8f36846c20
Jon Irazabal <jonirazabal14@gmail.com>
2016-04-22 13:20:17 +0200

    a

--- src/proiektua/advancedBattleship/ItsasoIbilgailua.java
+++ src/proiektua/advancedBattleship/ItsasoIbilgailua.java
@@ -1,5 +1,19 @@
 package proiektua.advancedBattleship;
 
-public class ItsasoIbilgailua extends Ibilgailua{
-
+public abstract class ItsasoIbilgailua extends ErasoMota{
+	private boolean egoera;
+	private int luzera;
+	private boolean egoera;
+	public ItsasoIbilgailua(int pPrezioa){
+		super(pPrezioa);
+		egoera=
+	}
+protected void espezifikazioakInprimatu(){
+	System.out.println(this.getIzena());
+	System.out.println("Prezioa: "+this.getPrezioa());
+	System.out.println("Prezioa: " +egoera);
+	System.out.println("Prezioa: " +luzera);
+}
+	
+protected abstract void informazioaInprimatu();
 }
