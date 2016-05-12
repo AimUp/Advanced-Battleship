430081a810490c1d02132abe37b4e8f127e9ddd1
AimUp <ugarteaimar@gmail.com>
2016-05-12 22:42:58 +0200

    estilo

--- src/proiektua/advancedBattleship/Main.java
+++ src/proiektua/advancedBattleship/Main.java
@@ -1,12 +1,26 @@
 package proiektua.advancedBattleship;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import proiektua.UI.IzenakPanelaUI;
 import proiektua.UI.Leihoa;
 
 public abstract class Main {
 
 	public static void main(String[] args) {
+		try {											//PROGRAMAREN ESTILO GRAFIKOA ALDATU
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
 		Leihoa.getLeihoa().panelaAldatu(new IzenakPanelaUI());
-		//AdvancedBattleship.getAdvancedBattleship().jokatu();
 	}
 }
