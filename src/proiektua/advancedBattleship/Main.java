f46d5244dee1fb762bb5b11658705f953b7e2377
Aimar <ugarteaimar@gmail.com>
2016-05-12 22:18:49 +0200

    no message

--- src/proiektua/advancedBattleship/Main.java
+++ src/proiektua/advancedBattleship/Main.java
@@ -1,13 +1,8 @@
 package proiektua.advancedBattleship;
 
-import proiektua.UI.IzenakPanelaUI;
-import proiektua.UI.Leihoa;
-
 public abstract class Main {
 
 	public static void main(String[] args) {
-		Leihoa.getLeihoa().panelaAldatu(new IzenakPanelaUI());
 		AdvancedBattleship.getAdvancedBattleship().jokatu();
-		
 	}
 }
